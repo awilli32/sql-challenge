c2f7fcec010cbc2d2a8a80f6951e73f0a137ad7f
Ashley <williamsash87@yahoo.com>
2021-03-14 20:25:51 -0400

    update

---
+++ schemas.sql
@@ -0,0 +1,52 @@
+Create Table employees (
+	emp_no     INT         NOT NUll,
+	birth_date DATE        NOT NULL,
+	first_name VARCHAR(14) NOT NULL, 
+	last_name  VARCHAR(16) NOT NULL,
+	sex        VARCHAR(1)  NOT NULL,
+	hire_date  DATE        NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+Create Table departments (
+	dept_no    VARCHAR(4)  NOT NULL,
+	dept_name  VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE      (dept_name)
+);
+	
+Create Table dept_manager (
+	dept_no  VARCHAR(4)  NOT NULL,
+	emp_no	 INT         NOT NULL,
+	from_date DATE       NOT NULL,
+	to_date  DATE 		 NOT NULL,
+	FOREIGN KEY (emp_no) References employees (emp_no),
+	FOREIGN KEY (dept_no) References departments (dept_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no    INT        NOT NULL,
+	dept_no    VARCHAR(4) NOT NULL,
+	from_date DATE       NOT NULL,
+    to_date   DATE       NOT NULL,
+	FOREIGN KEY (emp_no)  References employees  (emp_no),
+	FOREIGN KEY (dept_no)  References departments (dept_no),
+	PRIMARY KEY (emp_no,dept_no)
+);
+
+  CREATE TABLE titles (
+  emp_no INT  			NOT NULL,
+  title  VARCHAR (50) 	NOT NULL,
+  from_date DATE 		NOT NULL,
+  to_date   DATE,
+  FOREIGN KEY (emp_no) References employees (emp_no),
+  PRIMARY KEY (emp_no,title, from_date)
+);
+
+CREATE TABLE salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, form_date)
+);
